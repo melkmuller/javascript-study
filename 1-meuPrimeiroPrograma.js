1e2fd4b4446ece047a6d24269c8c15192ba336c8
Melki <melki.muller@gmail.com>
2022-06-22 17:21:45 -0300

    Primeiro commit

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,4 @@
+console.log("Meu primeiro programa");
+const idade = 29
+const nome = "Ricardo"
+let ano = 2020
---
+++ 3-operacoesAritmeticas.js
@@ -0,0 +1,12 @@
+console.log("Operações Aritméticas");
+console.log(2+2)
+console.log(10+8*2)
+console.log("ano" + 2020) // A soma não sabe que são números, pois você pode estar querendo fazer uma concatenação
+console.log("10" / "2") // Quando dividimos dois números que estão em string o JS entende como número e faz a operação
+console.log("*********************")
+
+console.log("Conversao de Tipos");
+console.log("ano" + 2020)
+console.log("2" + "2")
+console.log(parseInt("2") + parseInt("2"))
+console.log("*********************")
