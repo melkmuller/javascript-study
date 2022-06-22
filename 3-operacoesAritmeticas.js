76d48a5d20a5b4e9f9ee1ad17a8305e23ded33b0
Melki <melki.muller@gmail.com>
2022-06-22 17:40:11 -0300

    Altera arquivo 3 e adiciona arquivo 5

--- 3-operacoesAritmeticas.js
+++ 3-operacoesAritmeticas.js
@@ -3,6 +3,8 @@ console.log(2+2)
 console.log(10+8*2)
 console.log("ano" + 2020) // A soma não sabe que são números, pois você pode estar querendo fazer uma concatenação
 console.log("10" / "2") // Quando dividimos dois números que estão em string o JS entende como número e faz a operação
+console.log("Ricardo" / 2) // Retorna NaN 'not a number'
+console.log("7" / "2")
 console.log("*********************")
 
 console.log("Conversao de Tipos");
---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 29;
+let primeiroNome = "Melk";
+let sobrenome  = "Müller";
+
+// Vírgula funciona como espaço
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`)
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto)
+
+// let - pode variar o valor, receber outras atribuições
+// const - não pode ter o valor alterado, nem ser atribuído outro valor
